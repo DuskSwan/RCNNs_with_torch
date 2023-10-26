4711f10a0ad6f3b217b44cf62276772b5e2252da
DuskSwan <aquark@foxmail.com>
2023-10-26 16:59:00 +0800

    add my dataset class

--- data_manager.py
+++ data_manager.py
@@ -74,7 +74,81 @@ class PennFudanDataset(object):
     def __len__(self):
         return len(self.imgs)
     
+class hangcai03Dataset(object):
+    def __init__(self, root, transforms=None):
+        self.root = root # data root content
+        self.transforms = transforms
+        # load all image files, sorting them to ensure that they are aligned
+        self.imgs = list(sorted(os.listdir(os.path.join(root, "imgs")))) # img names
+        self.masks = list(sorted(os.listdir(os.path.join(root, "labels"))))
+
+    def __getitem__(self, idx):
+        # load images and masks
+        img_path = os.path.join(self.root, "imgs", self.imgs[idx])
+        mask_path = os.path.join(self.root, "labels", self.masks[idx])
+        img = Image.open(img_path).convert("RGB")
+        # note that we haven't converted the mask to RGB,
+        # because each color corresponds to a different instance
+        # with 0 being background
+        mask = Image.open(mask_path).convert('L')
+
+        mask = np.array(mask)
+        mask = (mask > 0).astype(int) # target label is 1, background is 0
+        # instances are encoded as different colors
+        obj_ids = np.unique(mask)
+        # first id is the background, so remove it
+        obj_ids = obj_ids[1:]
+
+        # split the color-encoded mask into a set of binary masks
+        masks = mask == obj_ids[:, None, None]
+
+        # get bounding box coordinates for each mask
+        num_objs = len(obj_ids)
+        boxes = []
+        for i in range(num_objs):
+            pos = np.where(masks[i])
+            xmin = np.min(pos[1])
+            xmax = np.max(pos[1])
+            ymin = np.min(pos[0])
+            ymax = np.max(pos[0])
+            boxes.append([xmin, ymin, xmax, ymax])
+
+        boxes = torch.as_tensor(boxes, dtype=torch.float32)
+        # there is only one class
+        labels = torch.ones((num_objs,), dtype=torch.int64)
+        masks = torch.as_tensor(masks, dtype=torch.uint8)
+
+        image_id = torch.tensor([idx])
+        area = (boxes[:, 3] - boxes[:, 1]) * (boxes[:, 2] - boxes[:, 0])
+        # suppose all instances are not crowd
+        iscrowd = torch.zeros((num_objs,), dtype=torch.int64)
+
+        target = {}
+        target["boxes"] = boxes
+        target["labels"] = labels
+        target["masks"] = masks
+        target["image_id"] = image_id
+        target["area"] = area
+        target["iscrowd"] = iscrowd
+
+        if self.transforms is not None:
+            img, target = self.transforms(img, target)
+
+        return img, target
+
+    def __len__(self):
+        return len(self.imgs)
+    
 if __name__ == '__main__':
-    cont = r'.\data\PennFudanPed'
-    dataset = PennFudanDataset(cont)
-    print(dataset[0])
+
+    # cont = r'.\data\PennFudanPed'
+    # dataset = PennFudanDataset(cont)
+    # print(dataset[0])
+
+    root = r'D:\Program\python\课题工作\23.10.10航材图像识别\hangcai03'
+    dataset = hangcai03Dataset(root)
+    img, target = dataset[0]
+    img.show()
+    mask = target['masks'][0].numpy()
+    stretched_mask = Image.fromarray(mask*255)
+    stretched_mask.show()
